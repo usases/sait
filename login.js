c7b54ef4b0feb317db4b67bdb88a174fd7475fc4
usases <160040682+usases@users.noreply.github.com>
2025-01-15 12:48:36 +0300

    Update login.js

--- login.js
+++ login.js
@@ -4,8 +4,17 @@ document.getElementById("login-form").addEventListener("submit", function(event)
   const username = document.getElementById("username").value;
   const password = document.getElementById("password").value;
 
+  // Получение сохраненных данных из LocalStorage
+  const storedUser = JSON.parse(localStorage.getItem("user"));
+
+  if (!storedUser) {
+    alert("Пользователь не зарегистрирован!");
+    return;
+  }
+
   // Проверка имени пользователя и пароля
-  if (username === "admin" && password === "admin") {
+  if (username === storedUser.username && password === storedUser.password) {
+    alert("Вы успешно вошли в систему!");
     window.location.href = "home.html"; // Перенаправление на главную страницу
   } else {
     alert("Неверное имя пользователя или пароль!");
